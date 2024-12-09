e60b71d29fc0c9ceff7320f3d7c18de6fce41942
Eraycan Sivri <eraycansivri@hotmail.com>
2024-12-09 18:31:15 +0300

    Update MessageType enum to include USERNAME_NOT_FOUND error code. Relates to #10.

--- src/main/java/com/kafka/exception/MessageType.java
+++ src/main/java/com/kafka/exception/MessageType.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum MessageType {
 
 	NO_RECORD_EXIST("1001", "kayıt bulunamadı"),
+	USERNAME_NOT_FOUND("1002", "username bulunamadı"),
 	GENERAL_EXCEPTION("9999", "genel bir hata oluştu");
 	
 	private String code;
