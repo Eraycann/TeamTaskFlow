0ce7dcfdd52213b8edc7d64a932cd4b2dd7b5669
Eraycan Sivri <eraycansivri@hotmail.com>
2024-12-09 18:27:30 +0300

    Add findByUsername method to UserRepository, relates to #10

--- src/main/java/com/kafka/repository/UserRepository.java
+++ src/main/java/com/kafka/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.kafka.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.kafka.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{ 
 
+	Optional<User> findByUsername(String username);
 }
