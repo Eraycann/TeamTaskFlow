78d45c1070371d69420ed1820b2c809ff792ee6d
Eraycan Sivri <eraycansivri@hotmail.com>
2024-12-10 16:43:04 +0300

    Add TOKEN_IS_EXPIRED to MessageType enum
    - Introduced `TOKEN_IS_EXPIRED` (code: 1005) for handling expired token errors.
    
    Relates to #10.

--- src/main/java/com/kafka/exception/MessageType.java
+++ src/main/java/com/kafka/exception/MessageType.java
@@ -7,6 +7,7 @@ public enum MessageType {
 
 	NO_RECORD_EXIST("1001", "kayıt bulunamadı"),
 	USERNAME_NOT_FOUND("1002", "username bulunamadı"),
+    TOKEN_IS_EXPIRED("1005", "tokenın süresi bitmiştir"),
 	GENERAL_EXCEPTION("9999", "genel bir hata oluştu");
 	
 	private String code;
